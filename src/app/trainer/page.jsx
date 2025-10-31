0b7a95f0378e4a3e6b604e64016ad8e31368f003
Keerthana Kanchi <kanchikeerthana98@gmail.com>
2025-10-30 21:51:31 -0400

    TrainerPageAdded

--- src/app/trainer/page.jsx
+++ src/app/trainer/page.jsx
@@ -1,12 +1,9 @@
 import Navbar from "../../components/common/Navbar";
 import Header from "../../components/home/Header";
 import Hero from "../../components/common/Hero";
-import FindSpecialities from "../../components/membership/FindSpecialities";
-import PlansSection from "../../components/membership/PlansSection";
-import ReviewsSection from "../../components/membership/ReviewsSection";
-import FeaturedTraining from "../../components/membership/FeaturedTraining";
 import FooterStrip from "@/components/common/FooterStrip";
-
+import FindSpecialtiesSection from "@/components/home/FindSpecialitiesSection";
+import TrainerList from "@/components/trainers/TrainerList";
 export const metadata = { title: "Membership | Depth Training" };
 
 export default function MembershipPage() {
@@ -14,10 +11,8 @@ export default function MembershipPage() {
     <main className="bg-black text-white min-h-screen w-full overflow-x-hidden">
       <Header />
       <Hero />
-      <FindSpecialities />
-      <PlansSection />
-      <ReviewsSection />
-      <FeaturedTraining />
+      <FindSpecialtiesSection />
+      <TrainerList />
       <FooterStrip /> {/* ✅ add footer */}
     </main>
   );
