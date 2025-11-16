a174feda2d469d83640be6d32c91a60b8ecd79f3
Md.Amanat khan shishir <shishirkhan95@gmail.com>
2025-11-16 15:11:16 -0500

    offer section added and hero section problem fixed

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,8 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import FloatingChatFab from "@/components/floatingchatfab/FloatingChatFab";
+import AnimationScroll from "@/components/common/AnimationScroll";
+import "aos/dist/aos.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
