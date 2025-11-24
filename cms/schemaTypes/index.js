e325162d8745ac1b9ceeb300420e00fb499f7d13
VignanCheepurupalli <vignan.ft05@gmail.com>
2025-11-24 00:23:17 -0500

    Added sanity cms schemas and initial setup

--- cms/schemaTypes/index.js
+++ cms/schemaTypes/index.js
@@ -1,1 +1,13 @@
-export const schemaTypes = []
+import post from './post'
+import serviceCategory from './serviceCategory'
+import service from './service'
+import membershipPlan from './membershipPlan'
+import testimonial from './testimonial'
+
+export const schemaTypes = [
+  post,
+  serviceCategory,
+  service,
+  membershipPlan,
+  testimonial,
+]
